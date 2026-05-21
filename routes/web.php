f94988773b866eb8d46dd8df5887c99f47b12f9b
bachti kh <kh.bachti@gmail.com>
2026-05-21 15:52:45 +0100

    handel m2m relationship

--- routes/web.php
+++ routes/web.php
@@ -18,11 +18,24 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 //CourseController
-Route::get('courses',[CourseController::class,'index'])->name('courses.index');
+/****************Version sans regroupement************* */
+/*Route::get('courses',[CourseController::class,'index'])->name('courses.index');
 Route::get('courses/add',[CourseController::class,'create'])->name('courses.create');
 Route::post('courses',[CourseController::class,'store'])->name('courses.store');
 Route::get('courses/{id}',[CourseController::class,'edit'])->name('courses.edit');
-Route::delete('courses',[CourseController::class,'destroy'])->name('courses.destroy');
+Route::delete('courses',[CourseController::class,'destroy'])->name('courses.destroy');*/
+/***************Version avec regroupement ********************** */
+Route::prefix('courses')
+    ->name('courses.')
+    ->controller(CourseController::class)
+    ->middleware(['auth', 'isTeacher']) // ajoute aussi isAdmin si besoin
+    ->group(function () {
+        Route::get('/', 'index')->name('index');
+        Route::get('/add', 'create')->name('create');
+        Route::post('/', 'store')->name('store');
+        Route::get('/{id}', 'edit')->name('edit');
+        Route::delete('/', 'destroy')->name('destroy');
+    });
 //*************Gérer la corbeille ***************/
 Route::get('/courses/trash', [CourseController::class,'trash'])->name('courses.trash');
 Route::post('/courses/{id}/restore', [CourseController::class,'restore'])->name('courses.restore');
